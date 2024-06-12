d42fe03063e2e6b9a9e810e0e2688c3b8947502b
ptolemy <ptolemy@ppro.local>
2024-06-12 14:08:38 +0200

    Wip ADC drivers - ADC reset

---
+++ application/launchpad_controller/modules/drivers/inc/ad4111.h
@@ -0,0 +1,17 @@
+/*	
+ *	AD4111 Analog to Digital Converter (ADC) Driver
+ *
+ *	Author: Love Mitteregger
+ *	Created: 10 June 2024
+ *
+ */
+
+// Preprocessor directive, if not defined then define...
+#ifndef AD4111_DRIVER_H
+#define AD4111_DRIVER_H
+
+void init_adc(void);
+void get_values_adc();
+void test(void);
+
+#endif
--- application/launchpad_controller/src/main.c
+++ application/launchpad_controller/src/main.c
@@ -1,6 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <ad4111.h>
+#include "ad4111.h"
 #include "networking.h"
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
