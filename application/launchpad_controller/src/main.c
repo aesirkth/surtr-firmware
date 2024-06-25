2e8e23054e3e03962a0bdf9eb89da22fc3141ad7
ptolemy <ptolemy@ppro.local>
2024-06-25 11:18:07 +0200

    Continued work on ad4111 drivers

--- application/launchpad_controller/src/main.c
+++ application/launchpad_controller/src/main.c
@@ -9,11 +9,25 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main() {
     LOG_INF("Hello world!");
+    
+    // Let's go..!
     LOG_INF("Starting ADC initializaiton...");
 
-    //const struct device *adc1 = DEVICE_DT_GET(DT_NODELABEL(ext_adc1));
-		//init_adc(adc1);
+    // Create adc driver instances
+    const struct device *adc_dev_0 = device_get_binding(DT_LABEL(DT_INST(0, ad4111)));
+    const struct device *adc_dev_1 = device_get_binding(DT_LABEL(DT_INST(1, ad4111)));
 
+    if (!adc_dev_0 || !adc_dev_1) {
+        LOG_ERR("Failed to get ADC device bindings");
+        return;
+    }
+
+    // Use the ADC devices
+    adc_init(adc_dev_0);
+    adc_init(adc_dev_1);
+
+    // Fail or success! 
     LOG_INF("ADC initialization complete");
+    // Application logic
 		return 0;
 }
