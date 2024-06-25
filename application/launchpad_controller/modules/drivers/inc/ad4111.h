6a5af469c096a3fdd94bc72d5fda7b7170c0bfe6
ptolemy <ptolemy@ppro.local>
2024-06-25 11:48:49 +0200

    Continued work on ad4111 drivers

--- application/launchpad_controller/modules/drivers/inc/ad4111.h
+++ application/launchpad_controller/modules/drivers/inc/ad4111.h
@@ -22,6 +22,7 @@
 #define ADC_DRIVER_API_H
 
 #include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
 
 /* Typedef declarations, functioning as aliases to their specific function pointer */
 typedef int (*adc_init_t) (const struct device *dev);
@@ -49,6 +50,7 @@ struct adc_api{
 struct adc_config{
     DEVICE_MMIO_ROM;
     adc_config_irq_t config_irq;
+    struct spi_dt_spec spi;
 };
 
 /* Subsystem functions that can be called on by the device via the API */
@@ -95,79 +97,3 @@ static inline void adc_handle_isr(const struct device *dev) {
 }
 
 #endif  // ADC_DRIVER_API_H#ifndef ADC_DRIVER_API_H
-#define ADC_DRIVER_API_H
-
-#include <zephyr/device.h>
-
-/* Typedef declarations, functioning as aliases to their specific function pointer */
-typedef int (*adc_init_t) (const struct device *dev);
-typedef int (*adc_reset_t) (const struct device *dev);
-typedef int (*adc_config_channel_t) (const struct device *dev, int channel, int config);
-typedef int (*adc_write_register_t) (const struct device *dev, uint8_t reg, uint16_t value);
-typedef int (*adc_read_register_t) (const struct device *dev, uint8_t reg, uint16_t *value);
-typedef int (*adc_read_data_t) (const struct device *dev, int channel, int *value);
-typedef void (*adc_config_irq_t) (const struct device *dev);
-typedef void (*adc_handle_isr_t) (const struct device *dev);
-
-/* A structure that functions as a device-independent-adc-API, 
- * applications will be able to program to this generic API */
-struct adc_api{
-    adc_init_t init;                     // Initialize ADC
-    adc_reset_t reset;                   // Reset ADC
-    adc_config_channel_t config_channel; // Enable/Disable ADC channel
-    adc_write_register_t write_register; // Write to ADC register
-    adc_read_register_t read_register;   // Read from ADC register
-    adc_read_data_t read_data;           // Read from ADC data register
-    adc_config_irq_t config_irq;         // Configurate IRQ
-    adc_handle_isr_t handle_isr;         // Handle ADC interrupt service routine
-};
-
-/* Subsystem functions that can be called on by the device via the API */
-static inline int adc_init(const struct device *dev) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->init(dev);
-}
-
-static inline int adc_reset(const struct device *dev) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->reset(dev);
-}
-
-static inline int adc_channel(const struct device *dev, int channel, int config) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->config_channel(dev, channel, config);
-}
-
-static inline int adc_write_register(const struct device *dev, uint8_t reg, uint16_t value) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->write_register(dev, reg, value);
-}
-
-static inline int adc_read_register(const struct device *dev, uint8_t reg, uint16_t *value) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->read_register(dev, reg, value);
-}
-
-static inline int adc_read_data(const struct device *dev, int channel, int *value) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    return api->read_data(dev, channel, value);
-}
-
-static inline void adc_config_irq(const struct device *dev) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    api->config_irq(dev);
-}
-static inline void adc_handle_isr(const struct device *dev) {
-    struct adc_api *api;
-    api = (struct adc_api *)dev->api;
-    api->handle_isr(dev);
-}
-
-#endif  // ADC_DRIVER_API_H
