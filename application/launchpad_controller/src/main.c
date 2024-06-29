5d735a640f4e5c95102a0b439bd6a498b031d979
ptolemy <ptolemy@ppro.local>
2024-06-29 13:28:50 +0200

    Trying emulation with zephyr native_sim board

--- application/launchpad_controller/src/main.c
+++ application/launchpad_controller/src/main.c
@@ -16,44 +16,52 @@ int main(void) {
     // Let's go..!
     LOG_INF("++ Starting ADC initialization ++");
 
+    if (!DT_NODE_HAS_STATUS(ADC1_NODE, okay)) {
+        LOG_ERR("ADC1 node is not okay");
+        return -1;
+    }
+    if (!DT_NODE_HAS_STATUS(ADC2_NODE, okay)) {
+        LOG_ERR("ADC2 node is not okay");
+        return -1;
+    }
     // Create adc driver instances
-    const struct device *adc1 = DEVICE_DT_GET(ADC1);
-    const struct device *adc2 = DEVICE_DT_GET(ADC2);
+    //const struct device *adc1 = DEVICE_DT_GET(ADC1);
+    //const struct device *adc2 = DEVICE_DT_GET(ADC2);
 
     /* Retrieve the device binding for the first ADC */
     // adc1 = device_get_binding("ad4111_1");
-    if (!device_is_ready(adc1)) {
-        LOG_INF("ad4111_1 failed to get binding");
-        return -1;  // Indicate error
-    } else {
-      LOG_INF("ad4111_1 successfully retrieved");
-    }
+    //if (!device_is_ready(adc1)) {
+    //    LOG_INF("ad4111_1 failed to get binding");
+    //    return -1;  // Indicate error
+    //} else {
+    //  LOG_INF("ad4111_1 successfully retrieved");
+    //}
 
     /* Retrieve the device binding for the second ADC */
-    adc2 = device_get_binding("ad4111@1"); // For debugging
+    //adc2 = device_get_binding("ad4111@1"); // For debugging
     // adc2 = device_get_binding("ad4111_2");
-    if (!device_is_ready(adc2)) {
-        LOG_ERR("ad4111_2 failed to get binding");
-        return -1;  // Indicate error
-    } else {
-        LOG_INF("ad4111_2 successfully retrieved");
-    }
+    //if (!device_is_ready(adc2)) {
+    //    LOG_ERR("ad4111_2 failed to get binding");
+    //    return -1;  // Indicate error
+    //} else {
+    //    LOG_INF("ad4111_2 successfully retrieved");
+    //}
 
     /* Initialize the first ADC */
-    if (adc_init(adc1) != 0) {
-        LOG_ERR("ad4111_1 failed to initialize");
-        return -1;  // Indicate error
-    } else {
-        LOG_INF("ad4111_1 successfully initialized");
-    }
+    //if (adc_init(adc1) != 0) {
+    //    LOG_ERR("ad4111_1 failed to initialize");
+    //    return -1;  // Indicate error
+    //} else {
+    //    LOG_INF("ad4111_1 successfully initialized");
+    //}
 
     /* Initialize the second ADC */
-    if (adc_init(adc2) != 0) {
-        LOG_ERR("ad4111_2 failed to initialize");
-        return -1;  // Indicate error
-    } else {
-        LOG_INF("ad4111_2 successfully initialized");
-    }
+    //if (adc_init(adc2) != 0) {
+    //    LOG_ERR("ad4111_2 failed to initialize");
+    //    return -1;  // Indicate error
+    //} else {
+    //    LOG_INF("ad4111_2 successfully initialized");
+    //}
 
     // Fail or success! 
     LOG_INF("++ Finished ADC initialization ++");
