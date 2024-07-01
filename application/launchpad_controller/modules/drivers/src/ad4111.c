d5afec265b8f2fbe3e940be2b382378a799cd263
ptolemy <ptolemy@ppro.local>
2024-07-01 16:45:13 +0200

    Trying emulation with zephyr native_sim board

--- application/launchpad_controller/modules/drivers/src/ad4111.c
+++ application/launchpad_controller/modules/drivers/src/ad4111.c
@@ -67,7 +67,7 @@ static int ad4111_reset(const struct device *dev) {
     return 0;
 }
 
-static int ad4111_init(const struct device *dedv) {
+static int ad4111_init(const struct device *dev) {
     const struct ad4111_config *config = dev->config;
     
     /* Do other initialization stuff */
