4bb61965ba9190b9bc043fe1e73f96065f1747e2
ptolemy <ptolemy@ppro.local>
2024-07-12 12:21:10 +0200

    Work on ADC instance init and definition macro

--- application/launchpad_controller/modules/drivers/src/ad4111.c
+++ application/launchpad_controller/modules/drivers/src/ad4111.c
@@ -113,7 +113,7 @@ static struct adc_api ad4111_api = {
     //    .channels = DT_INST_PROP(inst, channels),                   
 #define AD4111_DEVICE_DEFINE(inst)                                  \
     static const struct ad4111_config ad4111_config_##inst = {      \
-        .spi_max_frequency = DT_INST_PARENT(inst), \
+        .spi_max_frequency = DT_INST_PROP(DT_INST_PARENT(inst)), \
     };                                                              \
                                                                     \
     static struct ad4111_data ad4111_data_##inst;                   \
