d0843cbe147ed48b9f77ef5c421e52a6557533e6
Hugo Frisk <hugopaf@outlook.com>
2025-07-20 20:27:01 +0200

    plot all series and compensate for time overflow

--- dashboard/plot.py
+++ dashboard/plot.py
@@ -3,6 +3,8 @@ import sys
 import time
 import matplotlib.pyplot as plt
 
+print("parsing...")
+
 parser = SurtrParser(sys.argv[1])
 
 while True:
@@ -10,13 +12,88 @@ while True:
         break
     time.sleep(0.1)
 
-print("generating")
+print("generating...")
 
-v_start = 0.9
-v_end = 4.5
-field = "value50"
-convert = lambda x: 69 * (x - v_start) / (v_end - v_start)
-converted_values = [convert(x) for x in parser.data[field][1]]
+data = {
+    "m2": {
+        "start": 0.9,
+        "end": 4.5,
+        "scaler": 69,
+        "field": "value50",
+        "data_series": [],
+        "time_series": []
+    },
+    "m1": {
+        "start": 0.9,
+        "end": 4.5,
+        "scaler": 69,
+        "field": "value60",
+        "data_series": [],
+        "time_series": []
+    },
+    "m3": {
+        "start": 0.9,
+        "end": 4.5,
+        "scaler": 69,
+        "field": "value70",
+        "data_series": [],
+        "time_series": []
+    },
+    "e2": {
+        "start": 4,
+        "end": 20,
+        "scaler": 250,
+        "field": "value80",
+        "data_series": [],
+        "time_series": []
+    },
+    "e1": {
+        "start": 4,
+        "end": 20,
+        "scaler": 250,
+        "field": "value90",
+        "data_series": [],
+        "time_series": []
+    },
+    "e3": {
+        "start": 4,
+        "end": 20,
+        "scaler": 100,
+        "field": "value100",
+        "data_series": [],
+        "time_series": []
+    },
+    "e4": {
+        "start": 4,
+        "end": 20,
+        "scaler": 100,
+        "field": "value110",
+        "data_series": [],
+        "time_series": []
+    },
 
-plt.scatter(parser.data[field][0], converted_values)
+}
+
+for series in data.values():
+    series["data_series"] = [series["scaler"] * (x - series["start"]) / (series["end"] - series["start"]) for x in parser.data[series["field"]][1]]
+
+    series["time_series"] = [0] * len(parser.data[series["field"]][0])
+    # seems to be an overflow in the time counting so try to unfold the data by detecting if the time goes backwards
+    overflow_value = 0
+    for i in range(1, len(parser.data[series["field"]][0])):
+        if parser.data[series["field"]][0][i - 1] > parser.data[series["field"]][0][i]:
+            overflow_value += series["time_series"][i - 1]
+            print(f"time overflow at {overflow_value}")
+        series["time_series"][i] = parser.data[series["field"]][0][i] + overflow_value 
+
+plt.subplot(1, 2, 1)
+plt.plot(data["m2"]["time_series"], data["m2"]["data_series"])
+plt.plot(data["m1"]["time_series"], data["m1"]["data_series"])
+plt.plot(data["m3"]["time_series"], data["m3"]["data_series"])
+
+plt.subplot(1, 2, 2)
+plt.plot(data["e2"]["time_series"], data["e2"]["data_series"])
+plt.plot(data["e1"]["time_series"], data["e1"]["data_series"])
+plt.plot(data["e3"]["time_series"], data["e3"]["data_series"])
+plt.plot(data["e4"]["time_series"], data["e4"]["data_series"])
 plt.show()
